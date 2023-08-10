ac6a4b6d96e4457bf1488829eeb7f23effcc894e
Pedro Henrique Alves Veloso <contatopedrohalves@gmail.com>
2023-08-10 20:45:38 -0300

    feat: icon button

---
+++ src/lib/components/IconButton.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+  export let filled = false;
+  export let src: string;
+  export let alt = "Icon";
+</script>
+
+<button class={filled ? "filled" : ""}>
+  <img {src} {alt} />
+</button>
+
+<style>
+  button {
+    height: 60px;
+    width: 60px;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    border-radius: 20px;
+
+    border: solid var(--400) 4px;
+    background-color: transparent;
+  }
+
+  .filled {
+    background-color: var(--400);
+  }
+</style>
